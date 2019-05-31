0b00e37de52c68fdcf984a452ac450fce106c92d
Bart Smykla <bartek@smykla.com>
2019-05-31 10:39:27 +0100

    Moved backend config from versions.tf to backend.tf + new file name
    config.tf was renamed to main.tf
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- versions.tf
+++ versions.tf
@@ -1,9 +1,3 @@
 terraform {
   required_version = ">= 0.12"
-
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    region                      = "us-east-1"
-  }
 }
